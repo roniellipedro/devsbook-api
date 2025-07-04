8207b9a93a3f5c436d7782464ad8c4bcd22335f7
roni <roniellipedrocontato@gmail.com>
2025-07-04 07:28:21 -0300

    configurações iniciais no routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FeedController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ping', function () {
     return ['pong' => true];
 });
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+
+Route::post('/user', [UserController::class, 'create']);
+Route::put('/user', [UserController::class, 'update']);
+Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+Route::post('/user/cover', [UserController::class, 'updateCover']);
+
+Route::get('/feed', [FeedController::class, 'read']);
+Route::get('/user/feed', [FeedController::class, 'userFeed']);
+Route::get('/user/{id}/feed', [FeedController::class, 'userFeed']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::get('/user/{id}', [UserController::class], 'read');
