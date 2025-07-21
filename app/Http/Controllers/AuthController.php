52ce70b0e0beced5c22f6bd9d4a83b6fa49e6a50
roni <roniellipedrocontato@gmail.com>
2025-07-21 20:40:17 -0300

    adicionando a função de criar usuários com campoos validado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends BaseController
 {
-    public function __construct(){
-        $this->middleware('auth:api',[
-            'except' =>[
-            'login',
-            'create',
-            'unauthorized'
+    public function __construct()
+    {
+        $this->middleware('auth:api', [
+            'except' => [
+                'login',
+                'create',
+                'unauthorized'
             ]
         ]);
+    }
+
+    public function create(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthdate = $request->input('birthdate');
+
+        if ($name && $email && $password && $birthdate) {
+            if (strtotime($birthdate) === false) {
+                $array["error"] = "Data de nascimento inválida!";
+                return $array;
+            }
+
+            $emailExists = User::where("email", $email)->count();
+
+            if ($emailExists === 0) {
+
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User();
+
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->birthdate = $birthdate;
+                $newUser->save();
+
+                $token = Auth::attempt([
+                    "email" => $email,
+                    "password" => $password
+                ]);
+
+                if (!$token) {
+                    $array["error"] = "Ocorreu um erro!";
+                    return $array;
+                }
+
+                $array["token"] = $token;
+            } else {
+                $array["error"] = "Email já existe!";
+                return $array;
+            }
+        } else {
+            $array["error"] = "Não enviou todos os campos.";
+            return $array;
+        }
+
+        return $array;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,27 +11,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return ['pong' => true];
 });
-Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+// Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/logout', [AuthController::class, 'logout']);
-Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+// Route::post('/auth/login', [AuthController::class, 'login']);
+// Route::post('/auth/logout', [AuthController::class, 'logout']);
+// Route::post('/auth/refresh', [AuthController::class, 'refresh']);
 
-Route::post('/user', [UserController::class, 'create']);
-Route::put('/user', [UserController::class, 'update']);
-Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
-Route::post('/user/cover', [UserController::class, 'updateCover']);
+Route::post('/user', [AuthController::class, 'create']);
+// Route::put('/user', [UserController::class, 'update']);
+// Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+// Route::post('/user/cover', [UserController::class, 'updateCover']);
 
-Route::get('/feed', [FeedController::class, 'read']);
-Route::get('/user/feed', [FeedController::class, 'userFeed']);
-Route::get('/user/{id}/feed', [FeedController::class, 'userFeed']);
+// Route::get('/feed', [FeedController::class, 'read']);
+// Route::get('/user/feed', [FeedController::class, 'userFeed']);
+// Route::get('/user/{id}/feed', [FeedController::class, 'userFeed']);
 
-Route::get('/user', [UserController::class, 'read']);
-Route::get('/user/{id}', [UserController::class], 'read');
+// Route::get('/user', [UserController::class, 'read']);
+// Route::get('/user/{id}', [UserController::class], 'read');
 
-Route::get('/feed', [FeedController::class, 'create']);
+// Route::get('/feed', [FeedController::class, 'create']);
 
-Route::post('/post/{id}/like', [PostController::class, 'like']);
-Route::post('/post/{id}/comment', [PostController::class, 'comment']);
+// Route::post('/post/{id}/like', [PostController::class, 'like']);
+// Route::post('/post/{id}/comment', [PostController::class, 'comment']);
 
-Route::get('/search', [SearchController::class, 'search']);
+// Route::get('/search', [SearchController::class, 'search']);
