61f4865b52ccb8a2f0c0a96215e7bed38f3c48b2
Ronielli Pedro da Silva <roniellip4104_00@acicredi.local>
2025-07-22 15:42:40 -0300

    função update com os campos validados no UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
@@ -15,5 +16,64 @@ class UserController extends BaseController
         $this->middleware('auth:api');
 
         $this->loggedUser = Auth::user();
+    }
+
+    public function update(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $birthdate = $request->input('birthdate');
+        $city = $request->input('city');
+        $work = $request->input('work');
+        $password = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        $user = User::find($this->loggedUser['id']);
+
+        if ($name) {
+            $user->name = $name;
+        }
+
+        if ($email) {
+            if ($email != $user->email) {
+                $emailExists = User::where('email', $email)->count();
+                if ($emailExists === 0) {
+                    $user->email = $email;
+                } else {
+                    $array['error'] = 'E-mail já existe!';
+                    return $array;
+                }
+            }
+        }
+
+        if ($birthdate) {
+            if (strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválida!';
+                return $array;
+            }
+            $user->birthdate = $birthdate;
+        }
+
+        $user->city = $city;
+        $user->work = $work;
+
+        if ($password && $password_confirm) {
+            if ($password === $password_confirm) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $user->password = $hash;
+            } else {
+                $array['error'] = 'As senhas não se conferem.';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Preencha a senha duas vezes.';
+            return $array;
+        }
+
+        $user->save();
+
+        return $array;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::post('/auth/logout', [AuthController::class, 'logout']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh']);
 
 Route::post('/user', [AuthController::class, 'create']);
-// Route::put('/user', [UserController::class, 'update']);
+Route::put('/user', [UserController::class, 'update']);
 // Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
 // Route::post('/user/cover', [UserController::class, 'updateCover']);
 
